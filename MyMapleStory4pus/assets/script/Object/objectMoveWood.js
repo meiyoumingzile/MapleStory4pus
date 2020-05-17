ea46a76c18f2b119ab2f2127528d3617fa717a06
templete <39816793+meiyoumingzile@users.noreply.github.com>
2020-05-17 19:11:22 +0800

    1212
    sdsda

--- MyMapleStory4pus/assets/script/Object/objectMoveWood.js
+++ MyMapleStory4pus/assets/script/Object/objectMoveWood.js
@@ -10,32 +10,18 @@ cc.Class({
     onLoad: function () {
         this.body = this.node.getComponent(cc.RigidBody);
         this.body.linearVelocity=this.speed;
-        this.rangeX=[this.node.x-this.rangeSize.x/2,this.node.x+this.rangeSize.x/2];
-        this.rangeY=[this.node.y-this.rangeSize.y/2,this.node.y+this.rangeSize.y/2];
+        var sc=cc.v2(ALL.MainCanvas.width/2,ALL.MainCanvas.height/2);
+        this.rangeX=[Math.max(-sc.x,this.node.x-this.rangeSize.x/2),Math.min(sc.x,this.node.x+this.rangeSize.x/2)];
+        this.rangeY=[Math.max(-sc.y,this.node.y-this.rangeSize.y/2),Math.min(sc.y,this.node.y+this.rangeSize.y/2)];
     },
 
     
     update: function (dt) {
-        var speed=this.body.linearVelocity;
-		var a=false;
-		if(speed.x!=0&&(this.rangeX[0]>this.node.x||this.rangeX[1]<this.node.x)){
-			this.node.x=speed.x>0?this.rangeX[1]-0.01:this.rangeX[0]+0.01;
-			a=true;
+		if(this.rangeX[0]>this.node.x||this.rangeX[1]<this.node.x){
+			this.node.x=this.body.linearVelocity.x<0?this.rangeX[1]-0.01:this.rangeX[0]+0.01;
 		}
-		if(speed.y!=0&&(this.rangeY[0]>this.node.y||this.rangeY[1]<this.node.y)){
-			this.node.y=speed.y>0?this.rangeY[1]-0.01:this.rangeY[0]+0.01;
-			a=true;
-		}
-		if(a){
-            this.node.scaleX=-this.node.scaleX;
-            var __speed=cc.v2(speed.x,speed.y);
-			this.body.linearVelocity=cc.v2(0,0);
-			
-			
-			this.callbackAttack = function(){
-				this.body.linearVelocity=cc.v2(-__speed.x,-__speed.y);
-			}
-			this.schedule(this.callbackAttack,0.5,1,0);
+		if(this.rangeY[0]>this.node.y||this.rangeY[1]<this.node.y){
+			this.node.y=this.body.linearVelocity.y<0?this.rangeY[1]-0.01:this.rangeY[0]+0.01;
 		}
     },
 
